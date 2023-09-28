55bed80db4aef5bfcf9961e238b4b3cc229ddb1a
Devendra Singh Rao <dsrao1602@gmail.com>
2023-09-28 16:00:30 +0530

    "files restored"

---
+++ littlelemon/tests/test_models.py
@@ -0,0 +1,10 @@
+from django.test import TestCase
+
+from Restaurant.models import *
+
+
+class MenuItemTest(TestCase):
+    def test_get_item(self):
+        item = MenuItem.objects.create(
+            title="IceCream", price=80, inventory=100)
+        self.assertEqual(item, "IceCream:80")
